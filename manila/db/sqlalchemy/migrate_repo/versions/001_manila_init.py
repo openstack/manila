ee021608540f57a1cfc7aed19fcc98d37a7db4ef
Yulia Portnova <yportnova>
2013-09-02 15:05:13 +0300

    Modified migrations

--- manila/db/sqlalchemy/migrate_repo/versions/001_manila_init.py
+++ manila/db/sqlalchemy/migrate_repo/versions/001_manila_init.py
@@ -105,94 +105,6 @@ def upgrade(migrate_engine):
         mysql_engine='InnoDB'
     )
 
-    snapshots = Table(
-        'snapshots', meta,
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime),
-        Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
-        Column('id', String(length=36), primary_key=True, nullable=False),
-        Column('volume_id', String(length=36), nullable=False),
-        Column('user_id', String(length=255)),
-        Column('project_id', String(length=255)),
-        Column('status', String(length=255)),
-        Column('progress', String(length=255)),
-        Column('volume_size', Integer),
-        Column('scheduled_at', DateTime),
-        Column('display_name', String(length=255)),
-        Column('display_description', String(length=255)),
-        mysql_engine='InnoDB'
-    )
-
-    volume_types = Table(
-        'volume_types', meta,
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime),
-        Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
-        Column('id', Integer, primary_key=True, nullable=False),
-        Column('name', String(length=255)),
-        mysql_engine='InnoDB'
-    )
-
-    volume_metadata = Table(
-        'volume_metadata', meta,
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime),
-        Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
-        Column('id', Integer, primary_key=True, nullable=False),
-        Column('volume_id', String(length=36), ForeignKey('volumes.id'),
-               nullable=False),
-        Column('key', String(length=255)),
-        Column('value', String(length=255)),
-        mysql_engine='InnoDB'
-    )
-
-    volume_type_extra_specs = Table(
-        'volume_type_extra_specs', meta,
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime),
-        Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
-        Column('id', Integer, primary_key=True, nullable=False),
-        Column('volume_type_id', Integer, ForeignKey('volume_types.id'),
-               nullable=False),
-        Column('key', String(length=255)),
-        Column('value', String(length=255)),
-        mysql_engine='InnoDB'
-    )
-
-    volumes = Table(
-        'volumes', meta,
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime),
-        Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
-        Column('id', String(length=36), primary_key=True, nullable=False),
-        Column('ec2_id', String(length=255)),
-        Column('user_id', String(length=255)),
-        Column('project_id', String(length=255)),
-        Column('host', String(length=255)),
-        Column('size', Integer),
-        Column('availability_zone', String(length=255)),
-        Column('instance_uuid', String(length=36)),
-        Column('mountpoint', String(length=255)),
-        Column('attach_time', String(length=255)),
-        Column('status', String(length=255)),
-        Column('attach_status', String(length=255)),
-        Column('scheduled_at', DateTime),
-        Column('launched_at', DateTime),
-        Column('terminated_at', DateTime),
-        Column('display_name', String(length=255)),
-        Column('display_description', String(length=255)),
-        Column('provider_location', String(length=256)),
-        Column('provider_auth', String(length=256)),
-        Column('snapshot_id', String(length=36)),
-        Column('volume_type_id', Integer),
-        mysql_engine='InnoDB'
-    )
-
     quotas = Table(
         'quotas', meta,
         Column('id', Integer, primary_key=True, nullable=False),
@@ -206,34 +118,14 @@ def upgrade(migrate_engine):
         mysql_engine='InnoDB'
     )
 
-    iscsi_targets = Table(
-        'iscsi_targets', meta,
-        Column('created_at', DateTime),
-        Column('updated_at', DateTime),
-        Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
-        Column('id', Integer, primary_key=True, nullable=False),
-        Column('target_num', Integer),
-        Column('host', String(length=255)),
-        Column('volume_id', String(length=36), ForeignKey('volumes.id'),
-               nullable=True),
-        mysql_engine='InnoDB'
-    )
-
     # create all tables
     # Take care on create order for those with FK dependencies
     tables = [sm_flavors,
               sm_backend_config,
-              snapshots,
-              volume_types,
-              volumes,
-              iscsi_targets,
               migrations,
               quotas,
               services,
-              sm_volume,
-              volume_metadata,
-              volume_type_extra_specs]
+              sm_volume]
 
     for table in tables:
         try:
@@ -246,17 +138,11 @@ def upgrade(migrate_engine):
     if migrate_engine.name == "mysql":
         tables = ["sm_flavors",
                   "sm_backend_config",
-                  "snapshots",
-                  "volume_types",
-                  "volumes",
-                  "iscsi_targets",
                   "migrate_version",
                   "migrations",
                   "quotas",
                   "services",
-                  "sm_volume",
-                  "volume_metadata",
-                  "volume_type_extra_specs"]
+                  "sm_volume"]
 
         sql = "SET foreign_key_checks = 0;"
         for table in tables:
