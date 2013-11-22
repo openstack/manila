d24b76b4d7d1c6ae49b0ee147ff7f30c2507bd26
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2013-11-22 16:08:25 +0200

    Add devstack integration
    Add devstack integration for manila. Devstack has a plugin mechanism for
    unofficial projects to integrate with it without having to change any
    of devstack itself. This commit includes the files and some
    instructions on how to use them.
    
    Change-Id: I3b650b7696ccbcb7de9889a115e75310000774ca

---
+++ contrib/devstack/70-manila.sh
@@ -0,0 +1,30 @@
+# DevStack extras script to install Manila
+
+if is_service_enabled manila; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source
+        source $TOP_DIR/lib/manila
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        echo_summary "Installing Manila"
+        install_manila
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        echo_summary "Initialing Manila"
+        init_manila
+        echo_summary "Configuring Manila"
+        configure_manila
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        echo_summary "Starting Manila"
+        start_manila
+        echo_summary "Creating Manila entities for auth service"
+        create_manila_accounts
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+       cleanup_manila
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+       cleanup_manila
+       sudo rm -rf /etc/manila
+    fi
+fi
---
+++ contrib/devstack/README.rst
@@ -0,0 +1,18 @@
+====================
+Devstack Integration
+====================
+
+This directory contains the files necessary to integrate Manila with devstack.
+
+To install:
+
+$ DEVSTACK_DIR=/path/to/devstack
+$ cp lib/manila ${DEVSTACK_DIR}/lib
+$ cp extras.d/70-manila.sh ${DEVSTACK_DIR}/extras.d
+
+note: 70-manila.sh uses simple lvm-driver without multitenancy support.
+
+To configure devstack to run manila:
+
+$ cd ${DEVSTACK_DIR}
+$ services=(manila m-api m-shr m-sch); for item in ${services[*]}; do printf "%s\n" "enable_service $item" >> localrc; done
