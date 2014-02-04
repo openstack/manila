e8ade681b61ad3052d990907081a7427269de591
Aleks Chirko <achirko@mirantis.com>
2014-02-04 14:13:11 +0200

    Squash all migrations into one
    Migration #12 have fixed bug
    https://bugs.launchpad.net/manila/+bug/1272303,
    but introduced a new one. Because that bug
    was rather architectural, we need to address
    that architectural flaw instead of fixing
    bugs as they come.
    Closes-Bug: #1275044
    
    Change-Id: I260a90abd1107c62b687118d5d4a01d7199ea56f

--- manila/db/sqlalchemy/migrate_repo/versions/001_manila_init.py
+++ manila/db/sqlalchemy/migrate_repo/versions/001_manila_init.py
@@ -16,7 +16,7 @@
 
 from oslo.config import cfg
 from sqlalchemy import Boolean, Column, DateTime, ForeignKey
-from sqlalchemy import Integer, MetaData, String, Table
+from sqlalchemy import Integer, MetaData, String, Table, UniqueConstraint
 
 
 from manila.openstack.common import log as logging
@@ -35,7 +35,7 @@ def upgrade(migrate_engine):
         Column('created_at', DateTime),
         Column('updated_at', DateTime),
         Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
+        Column('deleted', Integer, default=0),
         Column('id', Integer, primary_key=True, nullable=False),
         Column('source_compute', String(length=255)),
         Column('dest_compute', String(length=255)),
@@ -44,7 +44,8 @@ def upgrade(migrate_engine):
         Column('instance_uuid', String(length=255)),
         Column('old_instance_type_id', Integer),
         Column('new_instance_type_id', Integer),
-        mysql_engine='InnoDB'
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
     )
 
     services = Table(
@@ -52,7 +53,7 @@ def upgrade(migrate_engine):
         Column('created_at', DateTime),
         Column('updated_at', DateTime),
         Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
+        Column('deleted', Integer, default=0),
         Column('id', Integer, primary_key=True, nullable=False),
         Column('host', String(length=255)),
         Column('binary', String(length=255)),
@@ -60,7 +61,8 @@ def upgrade(migrate_engine):
         Column('report_count', Integer, nullable=False),
         Column('disabled', Boolean),
         Column('availability_zone', String(length=255)),
-        mysql_engine='InnoDB'
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
     )
 
     quotas = Table(
@@ -69,18 +71,273 @@ def upgrade(migrate_engine):
         Column('created_at', DateTime),
         Column('updated_at', DateTime),
         Column('deleted_at', DateTime),
-        Column('deleted', Boolean),
+        Column('deleted', Integer, default=0),
         Column('project_id', String(length=255)),
         Column('resource', String(length=255), nullable=False),
         Column('hard_limit', Integer),
-        mysql_engine='InnoDB'
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
     )
 
+    quota_classes = Table(
+        'quota_classes', meta,
+        Column('created_at', DateTime(timezone=False)),
+        Column('updated_at', DateTime(timezone=False)),
+        Column('deleted_at', DateTime(timezone=False)),
+        Column('deleted', Integer, default=0),
+        Column('id', Integer(), primary_key=True),
+        Column('class_name',
+               String(length=255,
+                      convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False),
+               index=True),
+        Column('resource',
+               String(length=255,
+                      convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False)),
+        Column('hard_limit', Integer(), nullable=True),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+    )
+
+    quota_usages = Table(
+        'quota_usages', meta,
+        Column('created_at', DateTime(timezone=False)),
+        Column('updated_at', DateTime(timezone=False)),
+        Column('deleted_at', DateTime(timezone=False)),
+        Column('deleted', Integer, default=0),
+        Column('id', Integer(), primary_key=True),
+        Column('user_id', String(length=255)),
+        Column('project_id',
+               String(length=255, convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False),
+               index=True),
+        Column('resource',
+               String(length=255, convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False)),
+        Column('in_use', Integer(), nullable=False),
+        Column('reserved', Integer(), nullable=False),
+        Column('until_refresh', Integer(), nullable=True),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+        )
+
+    reservations = Table(
+        'reservations', meta,
+        Column('created_at', DateTime(timezone=False)),
+        Column('updated_at', DateTime(timezone=False)),
+        Column('deleted_at', DateTime(timezone=False)),
+        Column('deleted', Integer, default=0),
+        Column('id', Integer(), primary_key=True),
+        Column('user_id', String(length=255)),
+        Column('uuid',
+               String(length=36,
+                      convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False),
+               nullable=False),
+        Column('usage_id',
+               Integer(),
+               ForeignKey('quota_usages.id'),
+               nullable=False),
+        Column('project_id',
+               String(length=255, convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False),
+               index=True),
+        Column('resource',
+               String(length=255, convert_unicode=True,
+                      unicode_error=None,
+                      _warn_on_bytestring=False)),
+        Column('delta', Integer(), nullable=False),
+        Column('expire', DateTime(timezone=False)),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+        )
+
+    project_user_quotas = Table(
+        'project_user_quotas', meta,
+        Column('id', Integer, primary_key=True, nullable=False),
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', Integer, default=0),
+        Column('user_id', String(length=255), nullable=False),
+        Column('project_id', String(length=255), nullable=False),
+        Column('resource', String(length=25), nullable=False),
+        Column('hard_limit', Integer, nullable=True),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+        )
+
+    shares = Table(
+        'shares', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', String(length=36), default='False'),
+        Column('id', String(length=36), primary_key=True, nullable=False),
+        Column('user_id', String(length=255)),
+        Column('project_id', String(length=255)),
+        Column('host', String(length=255)),
+        Column('size', Integer),
+        Column('availability_zone',
+               String(length=255)),
+        Column('status', String(length=255)),
+        Column('scheduled_at', DateTime),
+        Column('launched_at', DateTime),
+        Column('terminated_at', DateTime),
+        Column('display_name', String(length=255)),
+        Column('display_description', String(length=255)),
+        Column('snapshot_id', String(length=36)),
+        Column('share_network_id',
+               String(length=36),
+               ForeignKey('share_networks.id'),
+               nullable=True),
+        Column('share_proto', String(255)),
+        Column('export_location', String(255)),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+        )
+
+    access_map = Table(
+        'share_access_map', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', String(length=36), default='False'),
+        Column('id', String(length=36), primary_key=True, nullable=False),
+        Column('share_id', String(36), ForeignKey('shares.id'),
+               nullable=False),
+        Column('access_type', String(255)),
+        Column('access_to', String(255)),
+        Column('state', String(255)),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+        )
+
+    share_snapshots = Table(
+        'share_snapshots', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', String(length=36), default='False'),
+        Column('id', String(length=36), primary_key=True, nullable=False),
+        Column('user_id', String(length=255)),
+        Column('project_id', String(length=255)),
+        Column('share_id', String(36), ForeignKey('shares.id'),
+               nullable=False),
+        Column('size', Integer),
+        Column('status', String(length=255)),
+        Column('progress', String(length=255)),
+        Column('display_name', String(length=255)),
+        Column('display_description', String(length=255)),
+        Column('share_size', Integer),
+        Column('share_proto', String(length=255)),
+        Column('export_location', String(255)),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+        )
+
+    share_metadata = Table(
+        'share_metadata', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', Integer, default=0),
+        Column('id', Integer, primary_key=True, nullable=False),
+        Column('share_id', String(length=36), ForeignKey('shares.id'),
+               nullable=False),
+        Column('key', String(length=255), nullable=False),
+        Column('value', String(length=1023), nullable=False),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+    )
+
+    security_services = Table(
+        'security_services', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', String(length=36), default='False'),
+        Column('id', String(length=36), primary_key=True, nullable=False),
+        Column('project_id', String(length=36), nullable=False),
+        Column('type', String(length=32), nullable=False),
+        Column('dns_ip', String(length=64), nullable=True),
+        Column('server', String(length=255), nullable=True),
+        Column('domain', String(length=255), nullable=True),
+        Column('sid', String(length=255), nullable=True),
+        Column('password', String(length=255), nullable=True),
+        Column('name', String(length=255), nullable=True),
+        Column('description', String(length=255), nullable=True),
+        Column('status', String(length=16)),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+    )
+
+    share_networks = Table(
+        'share_networks', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', String(length=36), default='False'),
+        Column('id', String(length=36), primary_key=True, nullable=False),
+        Column('project_id', String(length=36), nullable=False),
+        Column('neutron_net_id', String(length=36), nullable=True),
+        Column('neutron_subnet_id', String(length=36), nullable=True),
+        Column('network_type', String(length=32), nullable=True),
+        Column('segmentation_id', Integer, nullable=True),
+        Column('cidr', String(length=64), nullable=True),
+        Column('ip_version', Integer, nullable=True),
+        Column('name', String(length=255), nullable=True),
+        Column('description', String(length=255), nullable=True),
+        Column('status', String(length=32)),
+        UniqueConstraint('neutron_net_id', 'neutron_subnet_id', 'project_id',
+                         'deleted', name='net_subnet_uc'),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+    )
+
+    network_allocations = Table(
+        'network_allocations', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', String(length=36), default='False'),
+        Column('id', String(length=36), primary_key=True, nullable=False),
+        Column('ip_address', String(length=64), nullable=True),
+        Column('mac_address', String(length=32), nullable=True),
+        Column('share_network_id', String(length=36),
+               ForeignKey('share_networks.id'), nullable=False),
+        Column('status', String(length=32)),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+    )
+
+    ss_nw_association = Table(
+        'share_network_security_service_association', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('deleted', Integer, default=0),
+        Column('id', Integer, primary_key=True, nullable=False),
+        Column('share_network_id', String(length=36),
+               ForeignKey('share_networks.id'), nullable=False),
+        Column('security_service_id', String(length=36),
+               ForeignKey('security_services.id'), nullable=False),
+        mysql_engine='InnoDB',
+        mysql_charset='utf8',
+    )
     # create all tables
     # Take care on create order for those with FK dependencies
-    tables = [migrations,
-              quotas,
-              services]
+    tables = [migrations, quotas, services, quota_classes, quota_usages,
+              reservations, project_user_quotas, security_services,
+              share_networks, network_allocations, ss_nw_association,
+              shares, access_map, share_snapshots, share_metadata]
 
     for table in tables:
         try:
@@ -91,10 +348,12 @@ def upgrade(migrate_engine):
             raise
 
     if migrate_engine.name == "mysql":
-        tables = ["migrate_version",
-                  "migrations",
-                  "quotas",
-                  "services"]
+        tables = ["migrate_version", "migrations", "quotas", "services",
+                  "quota_classes", "quota_usages", "reservations",
+                  "project_user_quotas", "share_access_map", "share_snapshots",
+                  "share_metadata", "security_services", "share_networks",
+                  "network_allocations", "shares",
+                  "share_network_security_service_association"]
 
         sql = "SET foreign_key_checks = 0;"
         for table in tables:
@@ -107,4 +366,5 @@ def upgrade(migrate_engine):
 
 
 def downgrade(migrate_engine):
-    LOG.exception(_('Downgrade from initial Manila install is unsupported.'))
+    raise NotImplementedError('Downgrade from initial Manila install is not'
+                              ' supported.')
