db0726a2b4118661609a907c85382c474dd26e5e
vponomaryov <vponomaryov@mirantis.com>
2014-03-25 14:33:50 +0200

    Fixed detail list for shares
    Change-Id: Iba2a7cb66a5d17973db55c55ca723036992e754b
    Closes-bug: 1297205

--- manila/api/views/shares.py
+++ manila/api/views/shares.py
@@ -59,6 +59,8 @@ class ViewBuilder(common.ViewBuilder):
                 'status': share.get('status'),
                 'name': share.get('display_name'),
                 'description': share.get('display_description'),
+                'project_id': share.get('project_id'),
+                'host': share.get('host'),
                 'snapshot_id': share.get('snapshot_id'),
                 'share_network_id': share.get('share_network_id'),
                 'share_proto': share.get('share_proto'),
