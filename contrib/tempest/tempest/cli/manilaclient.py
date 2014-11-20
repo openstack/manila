3f750865762e7751ce0cbd4a171d68e9d1d5a8a6
Your Name <vponomaryov@mirantis.com>
2014-11-20 22:01:42 +0200

    Fix tempest compatibility for cli tests
    Commit https://github.com/openstack/tempest/commit/2474f41f made changes to
    tempest project that are inconsistent with our plugin.
    Make our plugin use latest changes to keep compatibility.
    
    Change-Id: I08d28b40fdd9ad54a0bcce30647d796943332116

--- contrib/tempest/tempest/cli/manilaclient.py
+++ contrib/tempest/tempest/cli/manilaclient.py
@@ -13,15 +13,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from tempest_lib.cli import base  # noqa
-
-from tempest import config_share as config
-from tempest import test
-
-CONF = config.CONF
+from tempest import cli
 
 
-class ClientTestBase(base.ClientTestBase, test.BaseTestCase):
+class ClientTestBase(cli.ClientTestBase):
 
     def manila(self, action, flags='', params='', fail_ok=False,
                endpoint_type='publicURL', merge_stderr=False):
@@ -29,13 +24,3 @@ class ClientTestBase(base.ClientTestBase, test.BaseTestCase):
         flags += ' --endpoint-type %s' % endpoint_type
         return self.clients.cmd_with_auth(
             'manila', action, flags, params, fail_ok, merge_stderr)
-
-    def _get_clients(self):
-        clients = base.CLIClient(
-            CONF.identity.admin_username,
-            CONF.identity.admin_password,
-            CONF.identity.admin_tenant_name,
-            CONF.identity.uri,
-            CONF.cli.cli_dir,
-        )
-        return clients
