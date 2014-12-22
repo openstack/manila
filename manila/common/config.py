1e069c6be4f6c94ab81af9f588af3c896f0e4360
Thomas Bechtold <tbechtold@suse.com>
2014-12-22 10:53:47 +0100

    Remove configs sql_connection and sql_connection_debug
    Both configuration options are available and used from the [database]
    section under the names 'connection' and 'connection_debug'.
    
    Change-Id: I7178ee9fce3b95c887d9b7e5d5564873aae69c88

--- manila/common/config.py
+++ manila/common/config.py
@@ -52,15 +52,6 @@ def _get_my_ip():
 
 
 core_opts = [
-    cfg.StrOpt('sql_connection',
-               default='sqlite:///$state_path/$sqlite_db',
-               help='The SQLAlchemy connection string used to connect to the '
-                    'database.',
-               secret=True),
-    cfg.IntOpt('sql_connection_debug',
-               default=0,
-               help='Verbosity of SQL debugging information: 0=None, '
-                    '100=Everything.'),
     cfg.StrOpt('api_paste_config',
                default="api-paste.ini",
                help='File name for the paste.deploy config for manila-api.'),
