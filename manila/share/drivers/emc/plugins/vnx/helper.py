d32c1e5c7086cfa9e611c3e14b2f780d8e94e051
Jay Xu <jay.xu@emc.com>
2015-06-10 22:41:09 -0400

    EMC VNX: Fix the total capacity for dynamic Pool
    For the pools that are not dynamic, the total capacity of the pool returns
    as total size of all members of the pool; For the pools that are dynamic,
    it returns the potential, maximum total size, in other words the total size
    of all members plus the space available for the pool extension.
    
    Change-Id: Ie093a9ebcda7ca77906efe3786ee585b836c6a52
    Closes-bug: 1464136

--- manila/share/drivers/emc/plugins/vnx/helper.py
+++ manila/share/drivers/emc/plugins/vnx/helper.py
@@ -613,7 +613,6 @@ class XMLAPIHelper(object):
                     'movers_id': [],
                     'virtualProvisioning': '',
                     'dataServicePolicies': '',
-                    'autoSize': '',
                     'greedy': '',
                     'isBackendPool': '',
                 }
@@ -623,7 +622,6 @@ class XMLAPIHelper(object):
                     'diskType',
                     'virtualProvisioning',
                     'dataServicePolicies',
-                    'autoSize',
                     'greedy',
                     'isBackendPool',
                 ]
@@ -631,7 +629,7 @@ class XMLAPIHelper(object):
 
                 pool['id'] = item[1].get('pool', '')
                 pool['used_size'] = item[1].get('usedSize', '')
-                pool['total_size'] = item[1].get('size', '')
+                pool['total_size'] = item[1].get('autoSize', '')
                 if 'movers' in item[1].keys():
                     pool['movers_id'] = item[1]['movers'].split()
 
