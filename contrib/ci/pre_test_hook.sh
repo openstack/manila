91ebe5bf65ba95d70f32fa96f59be53843bc58ce
Valeriy Ponomaryov <vponomaryov@mirantis.com>
2016-04-01 12:03:17 +0300

    Increase Cinder oversubscription ratio in CI
    Generic driver fails from time to time on Cinder not being able
    to schedule shares because of exceeded size limit.
    Since we do not use size in API tests at all increase
    Cinder oversubscription ratio from 20.0 to 100.0
    to make it enough for sure.
    
    Such error appears from time to time and
    more often with addon of new Tempest tests that create shares.
    
    TrivialFix
    
    Change-Id: I8a14f806c0235f5a12930eee362a838404cd5c06

--- contrib/ci/pre_test_hook.sh
+++ contrib/ci/pre_test_hook.sh
@@ -33,7 +33,7 @@ echo "IDENTITY_API_VERSION=3" >> $localrc_path
 # NOTE(vponomaryov): Set oversubscription ratio for Cinder LVM driver
 # bigger than 1.0, because in CI we do not need such small value.
 # It will allow us to avoid exceeding real capacity in CI test runs.
-echo "CINDER_OVERSUBSCRIPTION_RATIO=20.0" >> $localrc_path
+echo "CINDER_OVERSUBSCRIPTION_RATIO=100.0" >> $localrc_path
 echo "MANILA_BACKEND1_CONFIG_GROUP_NAME=london" >> $localrc_path
 echo "MANILA_BACKEND2_CONFIG_GROUP_NAME=paris" >> $localrc_path
 echo "MANILA_SHARE_BACKEND1_NAME=LONDON" >> $localrc_path
