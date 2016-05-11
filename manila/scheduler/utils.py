83c93c776707d5da9c89cde81c03022178127261
xing-yang <xing.yang@emc.com>
2016-05-11 15:23:31 -0400

    Check 'thin_provisioning' in extra specs
    Currently in the capacity filter and weigher of the scheduler,
    we use the logic to evaluate whether there is enough capacity
    to thin provision a share on a backend if the driver reports
    thin_provisioning to be True. However, a driver may be able to
    support both thin and thick provisioning. The logic does not
    check whether the user wants the share to be provisioned as thin
    or not.
    
    In this patch, we check 'thin_provisioning' in extra specs of the
    share type and decide whether to use the logic for thin or thick.
    In the following two cases, we will use the thin logic:
    1) 'thin_provisioning' is not set in extra specs. This is to
       provide backward compatibility.
    2) 'thin_provisioning' is set in extra specs and it is
       '<is> True' or 'True'.
    
    To provision a thick share on a backend that supports both
    thin and thick, set one of the following in extra specs:
        {'thin_provisioning': 'False'}
        {'thin_provisioning': '<is> False'}
        {'capabilities:thin_provisioning': 'False'}
        {'capabilities:thin_provisioning': '<is> False'}
    
    DocImpact
    Change-Id: I238a7962425ea35c356c5ed2e31b8f68462b3769
    Closes-Bug: #1578718

--- manila/scheduler/utils.py
+++ manila/scheduler/utils.py
@@ -1,4 +1,6 @@
 # Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+# Copyright (c) 2016 EMC Corporation
+#
 # All Rights Reserved.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -12,6 +14,10 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+from oslo_utils import strutils
+
+from manila.scheduler.filters import extra_specs_ops
 
 
 def generate_stats(host_state, properties):
@@ -61,3 +67,47 @@ def generate_stats(host_state, properties):
     }
 
     return stats
+
+
+def use_thin_logic(share_type):
+    # NOTE(xyang): To preserve the existing behavior, we use thin logic
+    # to evaluate in two cases:
+    # 1) 'thin_provisioning' is not set in extra specs (This is for
+    #    backward compatibility. If not set, the scheduler behaves
+    #    the same as before this bug fix).
+    # 2) 'thin_provisioning' is set in extra specs and it is
+    #    '<is> True' or 'True'.
+    # Otherwise we use the thick logic to evaluate.
+    use_thin_logic = True
+    thin_spec = None
+    try:
+        thin_spec = share_type.get('extra_specs', {}).get(
+            'thin_provisioning')
+        if thin_spec is None:
+            thin_spec = share_type.get('extra_specs', {}).get(
+                'capabilities:thin_provisioning')
+        # NOTE(xyang) 'use_thin_logic' and 'thin_provisioning' are NOT
+        # the same thing. The first purpose of "use_thin_logic" is to
+        # preserve the existing scheduler behavior if 'thin_provisioning'
+        # is NOT in extra_specs (if thin_spec is None, use_thin_logic
+        # should be True). The second purpose of 'use_thin_logic'
+        # is to honor 'thin_provisioning' if it is in extra specs (if
+        # thin_spec is set to True, use_thin_logic should be True; if
+        # thin_spec is set to False, use_thin_logic should be False).
+        use_thin_logic = strutils.bool_from_string(
+            thin_spec, strict=True) if thin_spec is not None else True
+    except ValueError:
+        # Check if the value of thin_spec is '<is> True'.
+        if thin_spec is not None and not extra_specs_ops.match(
+                True, thin_spec):
+            use_thin_logic = False
+    return use_thin_logic
+
+
+def thin_provisioning(host_state_thin_provisioning):
+    # NOTE(xyang): host_state_thin_provisioning is reported by driver.
+    # It can be either bool (True or False) or
+    # list ([True, False], [True], [False]).
+    thin_capability = [host_state_thin_provisioning] if not isinstance(
+        host_state_thin_provisioning, list) else host_state_thin_provisioning
+    return True in thin_capability
