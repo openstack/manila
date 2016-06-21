44a2d41797aaf575a3e4b8c9c0bbf4f94bee3aca
Marc Koderer <marc@koderer.com>
2016-06-21 16:56:06 +0200

    Change user_id and project_id to 255 length
    In case keystone is used with LDAP the user_id/project_id can be more
    than 60 characters. This will cause the issue that users cannot create
    any share-network or security_service.
    
    Change-Id: I2e2ccce32bf31850c9ffd74d9612cf5237d782fe
    Closes-bug: #1594824
    (cherry picked from commit 33ebd2742c9434ca1e196200d2389dba71f2d2d3)

---
+++ manila/db/migrations/alembic/versions/221a83cfd85b_change_user_project_id_length.py
@@ -0,0 +1,62 @@
+# Copyright 2016 SAP SE
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""change_user_id_length
+
+Revision ID: 221a83cfd85b
+Revises: eb6d5544cbbd
+Create Date: 2016-06-21 14:22:48.314501
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '221a83cfd85b'
+down_revision = 'eb6d5544cbbd'
+
+from alembic import op
+from oslo_log import log
+import sqlalchemy as sa
+
+from manila.i18n import _LI
+
+
+LOG = log.getLogger(__name__)
+
+
+def upgrade():
+    LOG.info(_LI("Changing user_id length for share_networks"))
+    op.alter_column("share_networks", "user_id",
+                    type_=sa.String(length=255))
+
+    LOG.info(_LI("Changing project_id length for share_networks"))
+    op.alter_column("share_networks", "project_id",
+                    type_=sa.String(length=255))
+
+    LOG.info(_LI("Changing project_id length for security_services"))
+    op.alter_column("security_services", "project_id",
+                    type_=sa.String(length=255))
+
+
+def downgrade():
+    LOG.info(_LI("Changing back user_id length for share_networks"))
+    op.alter_column("share_networks", "user_id",
+                    type_=sa.String(length=36))
+
+    LOG.info(_LI("Changing back project_id length for share_networks"))
+    op.alter_column("share_networks", "project_id",
+                    type_=sa.String(length=36))
+
+    LOG.info(_LI("Changing back project_id length for security_services"))
+    op.alter_column("security_services", "project_id",
+                    type_=sa.String(length=36))
