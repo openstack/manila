5ddbc047e7e8740782bd704606614629b064344a
melissaml <ma.lei@99cloud.net>
2017-02-08 17:14:50 +0800

    Use https instead of http for git.openstack.org
    Change-Id: I4ec2501d64103a430fdf4eaaf9044c4ff6b92682

--- install-guide/source/conf.py
+++ install-guide/source/conf.py
@@ -73,7 +73,7 @@ release = '0.1'
 #           extracted from git log.
 #   bug_tag: Tag for categorizing the bug. Must be set manually.
 # These variables are passed to the logabug code via html_context.
-giturl = u'http://git.openstack.org/cgit/openstack/manila/tree/install-guide/source'
+giturl = u'https://git.openstack.org/cgit/openstack/manila/tree/install-guide/source'
 git_cmd = "/usr/bin/git log | head -n1 | cut -f2 -d' '"
 gitsha = os.popen(git_cmd).read().strip('\n')
 # source tree
