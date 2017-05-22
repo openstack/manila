5bb153399b32ea208abed5701caff4795cbff11c
Victoria Martinez de la Cruz <victoria@redhat.com>
2017-05-22 01:13:17 -0300

    Add create/delete/extend/shrink share notifications
    This patchset adds notifications to share operations
    in manila. Precisely, create, delete, extend and shrink
    operations.
    
    Partially-Implements: blueprint ceilometer-integration
    
    Change-Id: I48324cf45536392576a2c29734b8f62a1f5676c9

--- manila/share/utils.py
+++ manila/share/utils.py
@@ -16,10 +16,13 @@
 
 """Share-related Utilities and helpers."""
 
-from manila.common import constants
+from oslo_config import cfg
 
+from manila.common import constants
+from manila import rpc
 
 DEFAULT_POOL_NAME = '_pool0'
+CONF = cfg.CONF
 
 
 def extract_host(host, level='backend', use_default_pool_name=False):
@@ -104,3 +107,41 @@ def cast_access_object_to_dict_in_readonly(rules):
             'access_to': rule['access_to']
         })
     return dict_rules
+
+
+def notify_about_share_usage(context, share, share_instance,
+                             event_suffix, extra_usage_info=None, host=None):
+
+    if not host:
+        host = CONF.host
+
+    if not extra_usage_info:
+        extra_usage_info = {}
+
+    usage_info = _usage_from_share(share, share_instance, **extra_usage_info)
+
+    rpc.get_notifier("share", host).info(context, 'share.%s' % event_suffix,
+                                         usage_info)
+
+
+def _usage_from_share(share_ref, share_instance_ref, **extra_usage_info):
+
+    usage_info = {
+        'share_id': share_ref['id'],
+        'user_id': share_ref['user_id'],
+        'project_id': share_ref['project_id'],
+        'snapshot_id': share_ref['snapshot_id'],
+        'share_group_id': share_ref['share_group_id'],
+        'size': share_ref['size'],
+        'name': share_ref['display_name'],
+        'description': share_ref['display_description'],
+        'proto': share_ref['share_proto'],
+        'is_public': share_ref['is_public'],
+        'availability_zone': share_instance_ref['availability_zone'],
+        'host': share_instance_ref['host'],
+        'status': share_instance_ref['status'],
+    }
+
+    usage_info.update(extra_usage_info)
+
+    return usage_info
