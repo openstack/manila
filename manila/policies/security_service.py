d7dd466a6d51370d843a0cf2cd6c0db52035ba42
Lance Bragstad <lbragstad@gmail.com>
2020-11-19 15:22:13 +0000

    Implement secure RBAC for security services
    This commit updates the policies for security services
    to understand scope checking and account for a read-only
    role. This is part of a broader series of changes across
    OpenStack to provide a consistent RBAC experience and
    improve security.
    
    Change-Id: I399a61691dad3a80c289c9f3f99f3c48be07846f
    Signed-off-by: Goutham Pacha Ravi <gouthampravi@gmail.com>

--- manila/policies/security_service.py
+++ manila/policies/security_service.py
@@ -10,6 +10,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+from oslo_log import versionutils
 from oslo_policy import policy
 
 from manila.policies import base
@@ -17,31 +18,75 @@ from manila.policies import base
 
 BASE_POLICY_NAME = 'security_service:%s'
 
+DEPRECATED_REASON = """
+The security service API now supports system scope and default roles.
+"""
+
+deprecated_security_service_create = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'create',
+    check_str=base.RULE_DEFAULT
+)
+deprecated_security_service_show = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'show',
+    check_str=base.RULE_DEFAULT
+)
+deprecated_security_service_detail = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'detail',
+    check_str=base.RULE_DEFAULT
+)
+deprecated_security_service_index = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'index',
+    check_str=base.RULE_DEFAULT
+)
+deprecated_security_service_update = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'update',
+    check_str=base.RULE_DEFAULT
+)
+deprecated_security_service_delete = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'delete',
+    check_str=base.RULE_DEFAULT
+)
+deprecated_security_service_get_all = policy.DeprecatedRule(
+    name=BASE_POLICY_NAME % 'get_all_security_services',
+    check_str=base.RULE_ADMIN_API
+)
+
 
 security_service_policies = [
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'create',
-        check_str=base.RULE_DEFAULT,
+        check_str=base.SYSTEM_ADMIN_OR_PROJECT_MEMBER,
+        scope_types=['system', 'project'],
         description="Create security service.",
         operations=[
             {
                 'method': 'POST',
                 'path': '/security-services'
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_create,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'show',
-        check_str=base.RULE_DEFAULT,
+        check_str=base.SYSTEM_OR_PROJECT_READER,
+        scope_types=['system', 'project'],
         description="Get details of a security service.",
         operations=[
             {
                 'method': 'GET',
                 'path': '/security-services/{security_service_id}'
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_show,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'detail',
-        check_str=base.RULE_DEFAULT,
+        check_str=base.SYSTEM_OR_PROJECT_READER,
+        scope_types=['system', 'project'],
         description="Get details of all security services.",
         operations=[
             {
@@ -52,10 +97,15 @@ security_service_policies = [
                 'method': 'GET',
                 'path': '/security-services/detail'
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_detail,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'index',
-        check_str=base.RULE_DEFAULT,
+        check_str=base.SYSTEM_OR_PROJECT_READER,
+        scope_types=['system', 'project'],
         description="Get all security services.",
         operations=[
             {
@@ -66,30 +116,45 @@ security_service_policies = [
                 'method': 'GET',
                 'path': '/security-services?{query}'
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_index,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'update',
-        check_str=base.RULE_DEFAULT,
+        check_str=base.SYSTEM_ADMIN_OR_PROJECT_MEMBER,
+        scope_types=['system', 'project'],
         description="Update a security service.",
         operations=[
             {
                 'method': 'PUT',
                 'path': '/security-services/{security_service_id}',
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_update,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'delete',
-        check_str=base.RULE_DEFAULT,
+        check_str=base.SYSTEM_ADMIN_OR_PROJECT_MEMBER,
+        scope_types=['system', 'project'],
         description="Delete a security service.",
         operations=[
             {
                 'method': 'DELETE',
                 'path': '/security-services/{security_service_id}'
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_delete,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
     policy.DocumentedRuleDefault(
         name=BASE_POLICY_NAME % 'get_all_security_services',
-        check_str=base.RULE_ADMIN_API,
+        check_str=base.SYSTEM_READER,
+        scope_types=['system'],
         description="Get security services of all projects.",
         operations=[
             {
@@ -100,7 +165,11 @@ security_service_policies = [
                 'method': 'GET',
                 'path': '/security-services/detail?all_tenants=1'
             }
-        ]),
+        ],
+        deprecated_rule=deprecated_security_service_get_all,
+        deprecated_reason=DEPRECATED_REASON,
+        deprecated_since=versionutils.deprecated.WALLABY
+    ),
 ]
 
 
