3a218a0d16ee3be6bbf5ecccc46c476ea906409f
Nahim Alves de Souza <nahimsouza@outlook.com>
2022-03-15 13:37:28 +0000

    [NetApp] Fixed replica promotion to updated autosize attributes
    When a volume is created in NetApp ONTAP, it has a few autosize
    attributes that are set by default. The values of the attributes
    are defined according to the volume type (DP/RW).
    
    During the replica promotion, the types are swaped between source
    and destination volumes, but the autosize values were not being
    updated. This patch fixes this behavior, calling an autosize reset
    after promoting the replica.
    
    Closes-Bug: #1957075
    Change-Id: I9a4e5763927b7585a8fbd6b0004d6a123dcd7fae
    (cherry picked from commit 8896534bb4860d6bf4ad7d6f016b9a38e85c9840)
    (cherry picked from commit b4ac0d89d160dd8e284327802ad891560be8548b)

--- manila/tests/share/drivers/netapp/dataontap/cluster_mode/test_lib_base.py
+++ manila/tests/share/drivers/netapp/dataontap/cluster_mode/test_lib_base.py
@@ -4427,7 +4427,8 @@ class NetAppFileStorageLibraryTestCase(test.TestCase):
         self.mock_object(self.library,
                          '_is_readable_replica',
                          mock.Mock(return_value=is_readable))
-
+        self.mock_object(self.library,
+                         '_update_autosize_attributes_after_promote_replica')
         replicas = self.library.promote_replica(
             None, [self.fake_replica, self.fake_replica_2],
             self.fake_replica_2, [], share_server=None)
@@ -4490,6 +4491,8 @@ class NetAppFileStorageLibraryTestCase(test.TestCase):
         self.mock_object(
             protocol_helper, 'cleanup_demoted_replica',
             mock.Mock(side_effect=exception.StorageCommunicationException))
+        self.mock_object(self.library,
+                         '_update_autosize_attributes_after_promote_replica')
         mock_log = self.mock_object(lib_base.LOG, 'exception')
 
         self.library.promote_replica(
@@ -4520,6 +4523,8 @@ class NetAppFileStorageLibraryTestCase(test.TestCase):
         self.mock_object(
             self.library, '_convert_destination_replica_to_independent',
             mock.Mock(side_effect=exception.StorageCommunicationException))
+        self.mock_object(self.library,
+                         '_update_autosize_attributes_after_promote_replica')
 
         replicas = self.library.promote_replica(
             None, [self.fake_replica, self.fake_replica_2],
@@ -4563,6 +4568,8 @@ class NetAppFileStorageLibraryTestCase(test.TestCase):
         self.mock_object(self.library,
                          '_is_readable_replica',
                          mock.Mock(return_value=False))
+        self.mock_object(self.library,
+                         '_update_autosize_attributes_after_promote_replica')
 
         replicas = self.library.promote_replica(
             None, [self.fake_replica, self.fake_replica_2, fake_replica_3],
@@ -4619,6 +4626,8 @@ class NetAppFileStorageLibraryTestCase(test.TestCase):
         self.mock_object(self.library,
                          '_is_readable_replica',
                          mock.Mock(return_value=False))
+        self.mock_object(self.library,
+                         '_update_autosize_attributes_after_promote_replica')
 
         replicas = self.library.promote_replica(
             None, [self.fake_replica, self.fake_replica_2],
@@ -4846,6 +4855,8 @@ class NetAppFileStorageLibraryTestCase(test.TestCase):
         self.mock_object(self.library,
                          '_is_readable_replica',
                          mock.Mock(return_value=False))
+        self.mock_object(self.library,
+                         '_update_autosize_attributes_after_promote_replica')
         replicas = self.library.promote_replica(
             None, [self.fake_replica, self.fake_replica_2],
             self.fake_replica_2, fake_access_rules, share_server=None)
