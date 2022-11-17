eb2abdeb3127fe019c9aa36876de5cc5cb5a6743
Maurice Escher <maurice.escher@sap.com>
2022-11-17 09:16:07 +0100

    [NetApp] guard already existing cifs access
    access user/group duplicate may contain NetBIOS Domain prefix
    
    e.g. in _handle_added_rules
    ```
    added_rules
    {
    'ntadmin': 'rw',
    'winadm': 'rw'
    }
    existing_rules
    {
    'D_WIN\ntadmin': 'full_control',
    'D_WIN\winadm': 'full_control'
    }
    new_rules
    {
    'ntadmin': 'rw',
    'winadm': 'rw'
    }
    ```
    
    Change-Id: I86d99bdbc75c3baaefdada0e9c8cc9d6389d3793

--- manila/share/drivers/netapp/dataontap/client/client_cmode.py
+++ manila/share/drivers/netapp/dataontap/client/client_cmode.py
@@ -3941,7 +3941,11 @@ class NetAppCmodeClient(client_base.NetAppBaseClient):
             'share': share_name,
             'user-or-group': user_name,
         }
-        self.send_request('cifs-share-access-control-create', api_args)
+        try:
+            self.send_request('cifs-share-access-control-create', api_args)
+        except netapp_api.NaApiError as e:
+            if e.code != netapp_api.EDUPLICATEENTRY:
+                raise
 
     @na_utils.trace
     def modify_cifs_share_access(self, share_name, user_name, readonly):
