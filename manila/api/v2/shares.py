318140e2505a946f7523e47dc6cf5b4ac34a7813
Goutham Pacha Ravi <gouthampravi@gmail.com>
2023-05-25 15:20:31 -0700

    [rbac] Pull up policy checks on share/snapshot APIs
    RBAC enforcement in manila happens in stages:
    1) Does user have access to the API
    2) Does user have access to the resource
    3) Is user permitted to perform the API action on the resource
    
    If (1) fails, user gets a HTTP 403, if (2) fails,
    they get a HTTP 404; if (3) fails, they get a HTTP 403.
    
    More often than not, (2) prevents "existence" detection
    of resources that don't belong to the user; except in
    case of "public" resources (e.g.: shares can be "public").
    
    In some share API methods, policy checks for (1) are
    happening after a bunch of processing. This leads to
    some inconsistency.
    
    Fix these occurrences to ensure a consistent user
    experience.
    
    Change-Id: I5b1f1ce517efed000f17b1e0901e183a1913ba9f
    Related-Bug: #2004230
    Signed-off-by: Goutham Pacha Ravi <gouthampravi@gmail.com>
    (cherry picked from commit 190876809f2bda4153166e055c9de9bd1786f2a8)

--- manila/api/v2/shares.py
+++ manila/api/v2/shares.py
@@ -242,6 +242,7 @@ class ShareController(wsgi.Controller,
 
     @wsgi.Controller.api_version('2.7')
     @wsgi.action('reset_status')
+    @wsgi.Controller.authorize('reset_status')
     def share_reset_status(self, req, id, body):
         context = req.environ['manila.context']
         try:
@@ -266,6 +267,7 @@ class ShareController(wsgi.Controller,
 
     @wsgi.Controller.api_version('2.69')
     @wsgi.action('soft_delete')
+    @wsgi.Controller.authorize('soft_delete')
     def share_soft_delete(self, req, id, body):
         """Soft delete a share."""
         context = req.environ['manila.context']
@@ -288,6 +290,7 @@ class ShareController(wsgi.Controller,
 
     @wsgi.Controller.api_version('2.69')
     @wsgi.action('restore')
+    @wsgi.Controller.authorize("restore")
     def share_restore(self, req, id, body):
         """Restore a share from recycle bin."""
         context = req.environ['manila.context']
@@ -578,6 +581,7 @@ class ShareController(wsgi.Controller,
         return self._revert(req, id, body)
 
     @wsgi.Controller.api_version("2.0")
+    @wsgi.Controller.authorize("get_all")
     def index(self, req):
         """Returns a summary list of shares."""
         if req.api_version_request < api_version.APIVersionRequest("2.35"):
@@ -598,6 +602,7 @@ class ShareController(wsgi.Controller,
         return self._get_shares(req, is_detail=False)
 
     @wsgi.Controller.api_version("2.0")
+    @wsgi.Controller.authorize("get_all")
     def detail(self, req):
         """Returns a detailed list of shares."""
         if req.api_version_request < api_version.APIVersionRequest("2.35"):
