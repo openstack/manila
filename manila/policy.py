503d7dfc61c6410a9d97c42c4d60ea54aa82e642
Ghanshyam Mann <gmann@ghanshyammann.com>
2024-08-09 11:59:21 -0700

    Remove default override for RBAC config options
    RBAC config options enforce_scope and enforce_new_defaults
    were disabled by default in oslo.policy and Manila had to override
    the default value to enable those by default. Now oslo.policy
    (4.4.0 onwards[1]) changed the default values[2] and enabled
    by default for all the services. OpenStack service does not need
    to override the default anymore.
    
    NOTE: There is no change in behaviour here, oslo.policy provides the
    same configuration that Manila has overridden till now.
    
    [1] https://review.opendev.org/c/openstack/releases/+/925032
    [2] https://review.opendev.org/c/openstack/oslo.policy/+/924283
    
    Depends-On: https://review.opendev.org/c/openstack/requirements/+/925464
    Change-Id: I1480dc81f1395989278852a9f215d90a90455cbb

--- manila/policy.py
+++ manila/policy.py
@@ -33,15 +33,13 @@ _ENFORCER = None
 
 
 # TODO(gmann): Remove setting the default value of config policy_file
-# once oslo_policy change the default value to 'policy.yaml'.
-# https://github.com/openstack/oslo.policy/blob/a626ad12fe5a3abd49d70e3e5b95589d279ab578/oslo_policy/opts.py#L49
+# 'policy_file' once oslo_policy change its default value to what
+# is overridden here.
 DEFAULT_POLICY_FILE = 'policy.yaml'
 opts.set_defaults(CONF, DEFAULT_POLICY_FILE)
 opts.set_defaults(
     cfg.CONF,
-    DEFAULT_POLICY_FILE,
-    enforce_scope=True,
-    enforce_new_defaults=True)
+    DEFAULT_POLICY_FILE)
 
 
 def reset():
